518f345eab1e9703d28494171cb4f3925ab629ef
jadengore <edward@bramanti.org>
2013-09-22 16:41:59 -0700

    Implemented function scramble()
    Still testing to see if scramble() is truly unbiased scramble method.

--- HW #2/JavascriptFunctions.js
+++ HW #2/JavascriptFunctions.js
@@ -15,3 +15,14 @@ function change(cents) {
 function strip_vowels(s) {
     return s.replace(/[aeiou]/ig, '');
 }
+
+function scramble(s) { //still testing to see if true, unbiased scramble.
+    result = s.split("");
+    for(var i = 0; i < s.length; i++) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var scrambler = result[i];
+        result[i] = result[j];
+        result[j] = scrambler;
+    }
+    return result.join("");
+}
