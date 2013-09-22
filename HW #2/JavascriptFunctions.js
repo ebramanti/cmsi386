039ddf46d56c23cc4a0b6c663732ba706dd643e0
jadengore <edward@bramanti.org>
2013-09-22 15:49:56 -0700

    Javascript HW

---
+++ HW #2/JavascriptFunctions.js
@@ -0,0 +1,17 @@
+function change(cents) {
+    //if cents < 0: raise ValueError("Not an int > 0.")
+    var usCoinDenominations = [25, 10, 5, 1];
+    var results = [];
+    var remaining = cents;
+    for (i = 0; i < usCoinDenominations.length; i++) {
+        s = usCoinDenominations[i];
+        partialSolution = [Math.floor(remaining / s), remaining % s];
+        results.push(partialSolution[0]);
+        remaining = partialSolution[1];
+    }
+    return results;
+}
+
+function strip_vowels(s){
+    return s.translate(None, "aeiouAEIOU")
+}
