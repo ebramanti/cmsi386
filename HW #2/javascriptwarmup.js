daa5905631da63d7251070c5fc42c0fcd09a1081
Edward Bramanti <edward@bramanti.org>
2013-09-24 02:08:53 -0700

    Added check for negative numbers in change()

--- HW #2/javascriptwarmup.js
+++ HW #2/javascriptwarmup.js
@@ -1,5 +1,7 @@
 function change(cents) {
-    //if cents < 0: raise ValueError("Not an int > 0.")
+    if (cents< 0) {
+        throw "Not an integer greater than 0.";
+    }
     //if cents isBoolean: raise ValueError("Input unrounded number of cents (float found)")
     var usCoinDenominations = [25, 10, 5, 1];
     var results = [];
