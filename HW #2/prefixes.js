6465d78253791acf7450702c361183af905bf2fb
Edward Bramanti <edward@bramanti.org>
2013-09-26 05:33:50 -0700

    Went with my version of prefixes()
    Due to HTML improvements, etc.

--- HW #2/prefixes.js
+++ HW #2/prefixes.js
@@ -1,13 +1,11 @@
-window.prefixes = function (word) {
-    var j = 0;
-    function next() {
-        if (j <= word.length) {
-            $("body").append("<div>" + word.substring(0, j) + "</div>");
-            setTimeout(function () {next();}, 1000);
-            j++;
-        }
-    }  
-    next();
+window.prefixes = function(word, i) {
+    if (i <= word.length) {
+        var substring = i === 0 ? "&nbsp;" : word.substring(0, i);
+        $("body").append('<div>' + substring + '</div>');
+        setTimeout(function () {
+            prefixes(word, i + 1);
+        }, 1000);
+    }
 }
 /*	Had to use window.prefixes to get prefixes noticed by the html
 	part of jsfiddle.
