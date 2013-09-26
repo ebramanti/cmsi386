3be0c37b3cce7c80417f6ea25a84cb08655c04d7
AuthorOfTheSurf <Mindfulness@Zanes-Mac-mini.local>
2013-09-26 01:48:59 -0700

    #300

--- HW #2/prefixes.js
+++ HW #2/prefixes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 window.prefixes = function (word) {
     var j = 0;
     function next() {
@@ -13,15 +12,3 @@ window.prefixes = function (word) {
 /*	Had to use window.prefixes to get prefixes noticed by the html
 	part of jsfiddle.
 */
-
-=======
-function prefixes(word, i) {
-    if (i <= word.length) {
-        var substring = i === 0 ? "&nbsp;" : word.substring(0, i);
-        $("body").append('<div>' + substring + '</div>');
-        setTimeout(function () {
-            prefixes(word, i + 1);
-        }, 1000);
-    }
-}
->>>>>>> a4f9e24fa140602487537c43bf3d317b7cbea66e
