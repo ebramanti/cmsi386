ba7abfeef347bcf4992b78e4fc071b59e0bb2a45
Edward Bramanti <edward@bramanti.org>
2013-10-13 19:23:13 -0700

    Implemented change() and removeVowels()

--- HW_3/gowarmup.go
+++ HW_3/gowarmup.go
@@ -1,45 +1,64 @@
 package gowarmup
 
 import (
-	"fmt"
-	"strings"
-	"strconv"
-	"regexp"
-	"container/list"
+    "fmt"
+    "strings"
+    "strconv"
+    "regexp"
+    "container/list"
+    "math"
 )
 
 func main() {
-	//mini test here
-
-	//..
+    //mini test here
+    change(20)
+    //..
 }
 
-func change(amount int) (change int) {
-	//
+func divmod(x, y int) (quotient, remainder int) {
+    quotient = x / y
+    remainder = x % y
+    return
+}
+
+func change(amount int) (change []int) {
+    changeLeft := amount
+    if (amount < 0 || amount % 1 != 0) {
+        //
+    }
+    quarters, changeLeft := divmod(changeLeft, 25)
+    dimes, changeLeft := divmod(changeLeft, 10)
+    nickels, changeLeft := divmod(changeLeft, 5)
+    pennies := changeLeft
+
+    result := []int{quarters, nickels, dimes, pennies}
+
+    return result
+
 }
 
 func removeVowels(s string) (t string) {
-	//
+    //
 }
 
 func scramble(s string) (t string) {
-	//
+    //
 }
 
 func powersOfTwo(upTo int)  {
-	//
+    //
 }
 
 func powers(upTo, base int) (result int) {
-	//
+    //
 }
 
 func interleave(a, b *List) (c List) {
-	//
+    //
 }
 
 func stutter(a, b *List) (c List) {
-	//
+    //
 }
 
 
