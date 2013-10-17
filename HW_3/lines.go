165a9477cfae1ebd7feeb85b23a67693e9ba1b99
Edward Bramanti <edward@bramanti.org>
2013-10-16 20:22:33 -0700

    lines.go officially working

--- HW_3/lines.go
+++ HW_3/lines.go
@@ -1,20 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"bufio"
+	"fmt"
+	"log"
+	"os"
 	"strings"
 )
 
 func main() {
-	filename, error := os.Open(os.Args[1]), ...
+	filename, error := os.Open(os.Args[1])
+	if error != nil {
+		log.Fatal(error)
+	}
 	scanner := bufio.NewScanner(filename)
 	count := 0
 	for scanner.Scan() {
 		line := strings.TrimSpace(scanner.Text())
-		if  line != "" && line[0] != '#' {
+		if line != "" && line[0] != '#' {
 			count += 1
 		}
 	}
+	fmt.Println(count)
 }
