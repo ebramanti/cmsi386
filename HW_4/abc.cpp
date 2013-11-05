7c3f7d21e9319622634e46f67ef47c2e6a98ae5a
AuthorOfTheSurf <Mindfulness@Zanes-Mac-mini.local>
2013-11-04 23:09:21 -0800

    Synced

---
+++ HW_4/abc.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int r = rand() % 2;
+    if (r == 0)
+    {
+        setup_a();
+    }
+    else if (r == 1)
+    {
+        setup_b();
+    }
+    else
+    { 
+        setup_c();
+    }
+    cout << magic_number << '\n';
+}
+
+void setup_a()
+{
+    static int magic_number = 8;
+}
+
+void setup_b()
+{
+    static int magic_number = 42;
+}
+
+void setup_c()
+{
+    static int magic_number = 777;
+}
+
+
+
