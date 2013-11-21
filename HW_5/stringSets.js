ed57040ee0ba2a7e3ceb83d217dacfc7e5b234d1
AuthorOfTheSurf <zanekansil@gmail.com>
2013-11-20 17:48:12 -0800

    some work to string sets js

---
+++ HW_5/stringSets.js
@@ -0,0 +1,21 @@
+//"\"Whatever...\""
+
+var letters = new RegExp([a-z], i);
+var 
+
+function doubleQuote(s) {
+    return true;
+}
+
+function pascalComment(s) {
+    return true;
+}
+
+function jsonNumber(s) {
+	return true;
+}
+
+function notHardcodedWords(s) {
+	var a = ["read", "red", "real"];
+	return true;
+}
