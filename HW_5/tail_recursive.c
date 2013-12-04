010fe89ecfe42fb65179b92a04dca101b818a2d6
Edward Bramanti <edward@bramanti.org>
2013-12-03 19:09:54 -0800

    Fixed C implementation, cleaned up .py & added .gitignore

--- HW_5/tail_recursive.c
+++ HW_5/tail_recursive.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 
-void findMinValue(int * anArray, int length) {
-	minValueHelper(anArray, length, 0, anArray[0]);
-}
-
 int minValueHelper (int * anArray, int length, int i, int sofar) {
     if (i == length - 1) {
-        return  (a[i] < sofar) ? a[i] : sofar;	
+        return  (anArray[i] < sofar) ? anArray[i] : sofar;	
     }
-    else if (a[i] < sofar) {
-        sofar = a[i]
+    else if (anArray[i] < sofar) {
+        sofar = anArray[i];
     }
     
-    return findMinValue(a, i + 1, sofar);
+    return minValueHelper(anArray, length, i + 1, sofar);
 }
 
-
+int findMinValue(int * anArray, int length) {
+	return minValueHelper(anArray, length, 0, anArray[0]);
+}
 
 int main() {
 	int a[3] = {1, 2, 3};
 	int b[4] = {21,7,12,2};
 	int c[4] = {1, 12, -1, 0};
 	int d[8] = {1, 20, 500, 6, -500, 40, -500, 67};
-	findMinValue(a, 3);
-	findMinValue(b, 4);
-	findMinValue(c, 4);
-	findMinValue(d, 8);
+	printf("%d\n", findMinValue(a, 3));
+	printf("%d\n", findMinValue(b, 4));
+	printf("%d\n", findMinValue(c, 4));
+	printf("%d\n", findMinValue(d, 8));
 }
