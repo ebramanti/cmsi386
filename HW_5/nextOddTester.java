a3b6abe5f7f30d63f7f32fea8c511d9b97c08452
Edward Bramanti <edward@bramanti.org>
2013-12-04 13:01:33 -0800

    Made a nextOdd java tester

---
+++ HW_5/nextOddTester.java
@@ -0,0 +1,9 @@
+public class nextOddTester {
+	public static void main(String[] args) {
+	    nextOdd a = new nextOdd();
+	    System.out.println(a.nextOddGenerator());
+	    System.out.println(a.nextOddGenerator());
+	    System.out.println(a.nextOddGenerator());
+	    System.out.println(a.nextOddGenerator());
+	}
+}
